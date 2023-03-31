9d36d99a6b5537d4bc3223afc51e98917ee5709f
ambell <ambell@mit.edu>
2023-03-31 15:54:26 -0400

    adding new files

---
+++ todomvc/src/components/Dot.svelte
@@ -0,0 +1,42 @@
+<script>
+
+    //import data from '../components/irregularviolence.json';
+
+
+    let hovered = -1;
+    let recorded_mouse_position = {
+		x: 0, y: 0
+	};
+    export let data_dot =[];
+    //console.log("Verifying output: ", Object.keys(data_person).length);
+    let data_values = Object.values(data_dot)
+
+</script>
+
+
+<main class="dots">
+    {#each data_values as individual, i}
+    <svg width=100 height=100 class="dot" viewBox="-100 -100 200 200">
+        <circle
+          cx="-0"
+          cy="-45"
+          r="28"
+          fill=	#808080
+          stroke=#808080
+          stroke-width="2"
+        />
+      </svg>
+      {/each}
+</main>
+
+<style>
+    .dot {
+        height: 20px;
+        width: 20px;
+        animation-name: floating;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-timing-function: ease-in-out;
+        animation-delay: 100ms;
+    }
+</style>
