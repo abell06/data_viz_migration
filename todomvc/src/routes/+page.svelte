7ea0b776d3b23e38e76c481010365d7d1f83c0be
ambell <ambell@mit.edu>
2023-05-15 12:16:25 -0400

    changed landing page

--- todomvc/src/routes/+page.svelte
+++ todomvc/src/routes/+page.svelte
@@ -1,30 +1,5 @@
 <script>
-    //import App from '../components/App.svelte';
+    import App_news from '../../components/App_news.svelte';
 </script>
 
-
-
-<main>
-    <h1>Entry page placeholder</h1>
-    <a href="./page2.html">View introduction to the data with a map (page 2)!</a>
-    <br>
-    <a href="./page3.html">View narrative on violence (page 3)</a>
-    <br>
-    <a href="./page4567.html">View guided data exploration (pages 4-7)</a>
-    <br>
-    <a href="./page8.html">View user interaction (page 8)</a>
-    <br>
-    <a href="./page9.html">View conclusion + call for action (page 9)</a>
-</main>
-
-<style>
-    main {
-        text-align: center;
-        font-family: Georgia;
-        font-weight: 300;
-        line-height: 2;
-        font-size: 24px;
-        color: var(--color-text);
-        margin-top: 100px;
-    }
-</style>
+<App_news />
